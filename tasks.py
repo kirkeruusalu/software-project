63dfbda1b5070b596445d914d2aeb672f6cbd4df
Kirke Ruusalu <kirke.ruusalu@gmail.com>
2024-11-19 19:14:10 +0200

    tasks

---
+++ tasks.py
@@ -0,0 +1,23 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/main.py", pty=True)
+
+@task
+def initialize(ctx):
+    ctx.run("python3 src/initialize.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage.html", pty=True)
+
+
