ec4300ec79944c3496f34697a5f4769abce69882
Kirke Ruusalu <kirke.ruusalu@gmail.com>
2024-11-19 18:21:17 +0200

    creation of files

---
+++ src/entities/user.py
@@ -0,0 +1,10 @@
+class User:
+    """
+    Class describing an individual user.
+
+    Attributes:
+        username (string): A unique username 
+    """
+
+    def __init__(self, username):
+        self.username = username
